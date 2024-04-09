f22ed62136ab45dc1597389830735dbc45588fcc
cqbent <cqbent@gmail.com>
2024-04-09 11:15:08 -0400

    add platform.sh settings

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -756,6 +756,11 @@ $settings['entity_update_backup'] = TRUE;
  */
 $settings['migrate_node_migrate_type_classic'] = FALSE;
 
+// Automatic Platform.sh settings.
+if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
+  include $app_root . '/' . $site_path . '/settings.platformsh.php';
+}
+
 // Automatically generated include for settings managed by ddev.
 if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 'true') {
   include __DIR__ . '/settings.ddev.php';

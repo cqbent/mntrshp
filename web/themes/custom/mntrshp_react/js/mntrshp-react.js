948e4d915c2147a95dd179fe4d52ec018a46c37b
cqbent <cqbent@gmail.com>
2024-05-24 10:23:03 -0400

    add modules, etc.

---
+++ web/themes/custom/mntrshp_react/js/mntrshp-react.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * mntrshp_react behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.mntrshpReact = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
